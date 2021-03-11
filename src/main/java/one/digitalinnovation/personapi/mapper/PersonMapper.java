4872c26cbc6033bc74b890e6637637f4f12a5c98
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2021-03-11 09:58:00 -0300

    Improved person creation functionality

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -1,12 +1,19 @@
 package one.digitalinnovation.personapi.mapper;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface PersonMapper {
 
-    //PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    //Person toModel(PersonDTO personDTO);
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
 
-    //PersonDTO toDTO(Person person);
+    PersonDTO toDTO(Person person);
 
 }
